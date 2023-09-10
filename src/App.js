dcdd1cc7537b9ae06767430b1b0cabaa757baa88
viralkansarav <viralkansarav@gmail.com>
2023-09-10 14:34:57 +0530

    added

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <h2>world</h2>
+    </div>
+  )
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Todo from './todo';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <Todo/>
+);
+
